8aa501029f0e2b0df8c36eb3ca2ad159b4ab92ef
arifka-rizki <rifka.rizki.fakhriza@gmail.com>
2021-07-11 11:02:19 +0700

    add gif command (#2)

---
+++ commands/fun/gif.js
@@ -0,0 +1,26 @@
+const fetch = require('node-fetch');
+const querystring = require('querystring');
+require('dotenv').config()
+
+module.exports = {
+    name: 'gif',
+    description: 'Send random gif based on keyword',
+    usage: '[keyword]',
+    args: true,
+    async execute(message, args,) {
+        try {
+            const response = await fetch(`https://g.tenor.com/v1/random?key=${process.env.TENORKEY}&q=${args.join(' ')}&limit=20`);
+            const data = await response.json();
+            if (data.length) {
+                return message.channel.send(`Sorry, I don't have ${args} gif collection`);
+            } else {
+                const index = Math.floor(Math.random() * 20);
+                const gifURL = data.results[index].media[0].gif.url;
+                message.channel.send(gifURL);
+            }
+        } catch (error) {
+            console.log(error);
+            message.channel.send(`An error occured\n${error}`);
+        }
+    }
+}
