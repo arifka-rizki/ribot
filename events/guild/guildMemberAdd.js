0f45e5556233d956fa5c27f7883481be3e16a121
arifka-rizki <rifka.rizki.fakhriza@gmail.com>
2021-07-26 11:59:06 +0700

    improve welcome message with custom image (#6)

--- events/guild/guildMemberAdd.js
+++ events/guild/guildMemberAdd.js
@@ -1,7 +1,47 @@
-module.exports = (Discord, client, member) => {
+const Canvas = require('canvas');
+
+const applyText = (canvas, text) => {
+    const context = canvas.getContext('2d');
+    let fontSize = 70;
+
+    do {
+        context.font = `${fontSize -= 10}px sans-serif`;
+    } while (context.measureText(text).width > canvas.width - 300);
+
+    return context.font;
+}
+
+module.exports = async (Discord, client, member) => {
     const channel = member.guild.channels.cache.get('852213233040621608');
     const welcomeRole = member.guild.roles.cache.get('856848545499578398');
-
+    if (!channel) return;
     member.roles.add(welcomeRole);
-    channel.send(`Welcome to the server ${member}`);
+
+    const canvas = Canvas.createCanvas(700, 250);
+    const context = canvas.getContext('2d');
+    const background = await Canvas.loadImage('./resources/wallpaper.jpg');
+
+    context.drawImage(background, 0, 0, canvas.width, canvas.height);
+
+    context.strokeStyle = '#74037b';
+    context.strokeRect(0, 0, canvas.width, canvas.height);
+
+    context.font = '28px sans-serif';
+    context.fillStyle = '#ffffff';
+    context.fillText('Welcome to the server,', canvas.width / 2.5, canvas.height / 3.5);
+
+    context.font = applyText(canvas, member.displayName);
+    context.fillStyle = '#ffffff';
+    context.fillText(member.displayName, canvas.width / 2.5, canvas.height / 1.8);
+
+    context.beginPath();
+    context.arc(125, 125, 100, 0, Math.PI * 2);
+    context.closePath();
+    context.clip();
+
+    const avatar = await Canvas.loadImage(member.user.displayAvatarURL({ format: 'jpg' }));
+    context.drawImage(avatar, 25, 25, 200, 200);
+
+    const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'welcome-image.png');
+    channel.send(``, attachment);
 }
